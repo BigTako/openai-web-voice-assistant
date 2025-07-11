9b7f7f65461e9354c1a4a3d91214c26163e1c191
BigTako <libert761@gmail.com>
2025-07-11 18:10:50 +0300

    ui finished

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 require('dotenv').config();
 const OpenAI = require('openai');
-const { OPENAI_API_KEY, ASSISTANT_ID } = process.env;
+const { OPENAI_API_KEY } = process.env;
 
 // Setup Express
 const app = express();
@@ -16,71 +16,10 @@ const openai = new OpenAI({
 });
 
 // Assistant can be created via API or UI
-const assistantId = ASSISTANT_ID;
-let pollingInterval;
 
 // ========================
 // OpenAI assistant section
 // ========================
-
-// Set up a Thread
-async function createThread() {
-  console.log('Creating a new thread...');
-  const thread = await openai.beta.threads.create();
-  return thread;
-}
-
-async function addMessage(threadId, message) {
-  console.log('Adding a new message to thread: ' + threadId);
-  const response = await openai.beta.threads.messages.create(threadId, {
-    role: 'user',
-    content: message,
-  });
-  return response;
-}
-
-async function runAssistant(threadId) {
-  console.log('Running assistant for thread: ' + threadId);
-  const response = await openai.beta.threads.runs.create(threadId, {
-    assistant_id: assistantId,
-    // Make sure to not overwrite the original instruction, unless you want to
-  });
-
-  console.log(response);
-
-  return response;
-}
-
-async function checkingStatus(res, threadId, runId) {
-  const runObject = await openai.beta.threads.runs.retrieve(threadId, runId);
-
-  const status = runObject.status;
-  console.log(runObject);
-  console.log('Current status: ' + status);
-
-  if (status == 'completed') {
-    clearInterval(pollingInterval);
-
-    const messagesList = await openai.beta.threads.messages.list(threadId);
-    const lastMessage = messagesList.body.data[0].content[0].text.value;
-
-    res.json({ message: lastMessage });
-  }
-}
-
-// ========================
-//       Route server
-// ========================
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// Open a new thread
-app.get('/thread', (req, res) => {
-  createThread().then((thread) => {
-    res.json({ threadId: thread.id });
-  });
-});
 
 const assistantInstruction = `
     You a support aget in real estate company. If user wants to search for real estate, please ask user to name such search criterias: count of bathrooms, count of bedrooms, minimal price, maximal price, minimal amount of parking spaces, maximal amount of parking spaces.
@@ -113,19 +52,6 @@ app.post('/message', async (req, res) => {
     responseId: response?.id,
   };
   res.status(200).json(result);
-  //   addMessage(threadId, message).then((message) => {
-  //     // res.json({ messageId: message.id });
-
-  //     // Run the assistant
-  //     runAssistant(threadId).then((run) => {
-  //       const runId = run.id;
-
-  //       // Check the status
-  //       pollingInterval = setInterval(() => {
-  //         checkingStatus(res, threadId, runId);
-  //       }, 500);
-  //     });
-  //   });
 });
 
 // Start the server
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,172 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { CheckIcon, XIcon } from '@phosphor-icons/react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css';
 
+type TMessageProps = {
+  content: string;
+  from: 'user' | 'bot';
+};
+
+function Message({ content, from }: TMessageProps) {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 5,
+      }}
+    >
+      <h3 style={{ margin: 0, textAlign: 'start' }}>
+        {from === 'user' ? 'You' : '🤖 Bot'}
+      </h3>
+      <div
+        style={{
+          textAlign: 'start',
+          padding: 10,
+          backgroundColor: from === 'user' ? '#303030' : 'transparent',
+          borderRadius: 10,
+        }}
+      >
+        {content}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+function App() {
+  // const [count, setCount] = useState(0)
+  const chatHistory = [
+    {
+      from: 'user',
+      content:
+        'i receive a stream of text(chunks) on front-end from backend, can i sound it out by chunks using openai library fro node.js?',
+    },
+    {
+      from: 'bot',
+      content:
+        'Yes—you can have the OpenAI Node.js SDK spit out audio as soon as each chunk arrives, so you don’t have to wait for the full file before playing it.',
+    },
+    {
+      from: 'user',
+      content: `Is there a separate instruction for incrementing the instruction address register in the CPU or it's done automaticaly when current instruction is executed? If automaticaly and the current instruction is JUMP, it has to overwrite the current instruction address, and then increment it since the JUMP instruction is executed, so it works weird`,
+    },
+    {
+      from: 'bot',
+      content:
+        'There is no explicit “INC PC” instruction in almost any real CPU ISA; PC (or IP — Instruction Pointer) updating is baked into the fetch/decode hardware. Here’s roughly what happens each cycle:',
+    },
+  ] as { from: 'user' | 'bot'; content: string }[];
+
+  const [isRecording, setIsRecording] = useState(false);
+  const [isSubmittingRecording, setIsSubmittingRecording] = useState(false);
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          width: '50%',
+          flexDirection: 'column',
+          gap: 20,
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+            height: 500,
+            overflowY: 'scroll',
+            padding: 10,
+            gap: 20,
+          }}
+        >
+          {chatHistory.map((message) => (
+            <Message content={message.content} from={message.from} />
+          ))}
+        </div>
+        {isRecording ? (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 5,
+              backgroundColor: '#303030',
+              width: '100%',
+              borderRadius: 20,
+              padding: 10,
+            }}
+          >
+            <div>Sheech visualizer</div>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                marginLeft: 'auto',
+                gap: 10,
+              }}
+            >
+              <button
+                title='Cancel recording'
+                onClick={() => setIsRecording(false)}
+                style={{
+                  backgroundColor: 'transparent',
+                  borderRadius: '100%',
+                  padding: 7,
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <XIcon size={20} />
+              </button>
+              <button
+                title='Submit speech'
+                style={{
+                  backgroundColor: 'transparent',
+                  borderRadius: '100%',
+                  padding: 7,
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+                onClick={() => setIsSubmittingRecording(true)}
+              >
+                {isSubmittingRecording ? (
+                  <span className='loader'></span>
+                ) : (
+                  <CheckIcon size={20} />
+                )}
+              </button>
+            </div>
+          </div>
+        ) : (
+          <button
+            style={{
+              width: '100%',
+              backgroundColor: '#00FF7F',
+              padding: 10,
+              borderRadius: 20,
+            }}
+            onClick={() => setIsRecording(true)}
+          >
+            <span style={{ color: '#303030' }}>
+              <strong>Start recording</strong>
+            </span>
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
